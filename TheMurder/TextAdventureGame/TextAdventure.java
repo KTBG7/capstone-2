0642f67b3162cdc0500683f21385d18c60a8d204
Kevin Ibanez <kpeibanez@gmail.com>
2020-07-13 00:27:31 -0500

    finally fixed the game!

--- TheMurder/TextAdventureGame/TextAdventure.java
+++ TheMurder/TextAdventureGame/TextAdventure.java
@@ -3,14 +3,6 @@ package TheMurder.TextAdventureGame;
 import java.util.Scanner;
 import java.io.File;
 
-class Choice {
-    int choiceNumber;
-    String choiceName;
-    String description;
-    int numExits;
-    String[] exits = new String[35];
-    int[] destinations = new int[35];
-}
 
 public class TextAdventure {
 
